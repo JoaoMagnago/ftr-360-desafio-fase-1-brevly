79930ad0e34d7cbc460e70dd0a1cbfdb8aac0e92
joao magnago <joaomagnago.wm@gmail.com>
2025-09-17 09:26:49 -0300

    fix: update short url format in test and clean up before each test

--- server/src/app/functions/create-short-link.spec.ts
+++ server/src/app/functions/create-short-link.spec.ts
@@ -1,16 +1,21 @@
-import { randomUUID } from 'node:crypto'
 import { eq } from 'drizzle-orm'
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
 import { isRight } from '@/infra/shared/either'
 
 describe('create short link', () => {
-  it('should be able to create a short link and get its id', async () => {
-    const namePattern = randomUUID().toLowerCase().slice(0, 10)
-    const originalUrl = `https://example.com/${namePattern}`
-    const shortUrl = namePattern
+  const namePattern = 'Example-Page1'
+  const originalUrl = `https://example.com/${namePattern}`
+  const shortUrl = namePattern
 
+  beforeEach(() => {
+    return db
+      .delete(schema.shortLinks)
+      .where(eq(schema.shortLinks.shortUrl, shortUrl))
+  })
+
+  it('should be able to create a short link and get its id', async () => {
     const { createShortLink } = await import('./create-short-link')
 
     const result = await createShortLink({ originalUrl, shortUrl })
