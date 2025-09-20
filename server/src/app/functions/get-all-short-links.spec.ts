b538fbbcd8edaf493d43faa5fbcd006066850c2b
joao magnago <joaomagnago.wm@gmail.com>
2025-09-19 21:19:53 -0300

    test: create test for get all short links

---
+++ server/src/app/functions/get-all-short-links.spec.ts
@@ -0,0 +1,33 @@
+import { uuidv7 } from 'uuidv7'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { isRight, unwrapEither } from '@/infra/shared/either'
+import { createShortLink } from './create-short-link'
+import { getAllShortLinks } from './get-all-short-links'
+
+describe('get short link', () => {
+  beforeEach(async () => {
+    await db.delete(schema.shortLinks)
+  })
+
+  it('should be able to get all short link from database', async () => {
+    const shortUrls = [
+      `Example-Page${uuidv7().slice(0, 6).toLowerCase()}`,
+      `Example-Page1${uuidv7().slice(0, 6).toLowerCase()}`,
+    ]
+    const originalUrl = 'https://example.com'
+
+    shortUrls.forEach(async shortUrl => {
+      await createShortLink({ originalUrl, shortUrl })
+    })
+
+    const getAllShortLinksSut = await getAllShortLinks()
+
+    expect(isRight(getAllShortLinksSut)).toBe(true)
+    if (isRight(getAllShortLinksSut)) {
+      const shortLinks = unwrapEither(getAllShortLinksSut).shortLinks
+      expect(shortLinks.length).toBe(2)
+    }
+  })
+})
