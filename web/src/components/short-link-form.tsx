9eebd186d4f7fd9f51978ab852a6be24aea64d03
joao magnago <joaomagnago.wm@gmail.com>
2025-09-25 10:42:52 -0300

    feat(short-link-form): finish component's general styles

--- web/src/components/short-link-form.tsx
+++ web/src/components/short-link-form.tsx
@@ -1,23 +1,39 @@
 export function ShortLinkForm() {
   return (
     <div className="flex flex-col p-8 gap-6 bg-gray-100 max-w-[380px] w-full rounded-lg">
-      <h2 className="text-lg text-gray-600">Novo link</h2>
+      <h2 className="text-xl/8 font-(weight:--font-bold) text-gray-600">
+        Novo link
+      </h2>
 
       <div className="flex flex-col gap-4">
-        <span className="text-gray-500 text-xs uppercase">Link original</span>
-        <input
-          className="h-12 w-full px-4 py[0.9375rem] text-gray-600 placeholder:text-gray-400 text-md font-regular"
-          placeholder="www.exemplo.com.br"
-        />
+        <div className="flex flex-col gap-2">
+          <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
+            Link original
+          </span>
+          <input
+            className="h-12 w-full px-4 py[0.9375rem] text-md text-gray-600 border-[1px] border-gray-300 rounded-lg focus-visible:outline-none placeholder:text-gray-400"
+            placeholder="www.exemplo.com.br"
+            spellCheck={false}
+          />
+        </div>
 
-        <span className="text-gray-500 text-xs uppercase">Link original</span>
-        <input
-          className="h-12 w-full px-4 py[0.9375rem] text-gray-600 placeholder:text-gray-400 text-md font-regular"
-          placeholder="brev.ly"
-        />
+        <div className="flex flex-col gap-2">
+          <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
+            Link encurtado
+          </span>
+          <div className="flex align-center h-12 w-full px-4 py[0.9375rem] text-md border-[1px] border-gray-300 rounded-lg">
+            <span className="self-center h-fit text-gray-600">brev.ly/</span>
+            <input
+              className="w-full text-gray-600 focus-visible:outline-none"
+              spellCheck={false}
+            />
+          </div>
+        </div>
       </div>
 
-      <button className="bg-blue-base text-white text-md">Salvar link</button>
+      <button className="h-12 text-md text-white font-(weight:--font-semibold) cursor-pointer bg-blue-base rounded-lg">
+        Salvar link
+      </button>
     </div>
   );
 }
