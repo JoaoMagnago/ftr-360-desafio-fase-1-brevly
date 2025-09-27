292aaf71530c97d65b13c36eb68af86bac5c347a
joao magnago <joaomagnago.wm@gmail.com>
2025-09-26 23:58:37 -0300

    feat(short-link-form): implement react hook form with zod validation

--- web/src/components/short-link-form.tsx
+++ web/src/components/short-link-form.tsx
@@ -1,4 +1,35 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import z from 'zod';
+
+const createShortLinkSchema = z.object({
+  originalUrl: z.url(),
+  shortUrl: z
+    .string()
+    .min(1)
+    .max(50)
+    .regex(/^[a-zA-Z0-9-_]+$/),
+});
+
+type CreateShortLinkFormValues = z.infer<typeof createShortLinkSchema>;
+
 export function ShortLinkForm() {
+  const { control, watch } = useForm<CreateShortLinkFormValues>({
+    resolver: zodResolver(createShortLinkSchema),
+    defaultValues: {
+      originalUrl: '',
+      shortUrl: '',
+    },
+  });
+
+  const originalUrl = watch('originalUrl');
+  const shortUrl = watch('shortUrl');
+
+  useEffect(() => {
+    console.log({ originalUrl, shortUrl });
+  }, [originalUrl, shortUrl]);
+
   return (
     <div className="flex flex-col p-8 gap-6 bg-gray-100 max-w-[380px] w-full rounded-lg">
       <h2 className="text-xl/8 font-(weight:--font-bold) text-gray-600">
@@ -6,32 +37,53 @@ export function ShortLinkForm() {
       </h2>
 
       <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
-            Link original
-          </span>
-          <input
-            className="h-12 w-full px-4 py[0.9375rem] text-md text-gray-600 border-[1px] border-gray-300 rounded-lg focus-visible:outline-none placeholder:text-gray-400"
-            placeholder="www.exemplo.com.br"
-            spellCheck={false}
-          />
-        </div>
+        <Controller
+          control={control}
+          name={'originalUrl'}
+          render={({ field: { value, onChange } }) => (
+            <div className="flex flex-col gap-2">
+              <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
+                Link original
+              </span>
+              <input
+                value={value}
+                className="h-12 w-full px-4 py[0.9375rem] text-md text-gray-600 border-[1px] border-gray-300 rounded-lg focus-visible:outline-none placeholder:text-gray-400"
+                placeholder="www.exemplo.com.br"
+                spellCheck={false}
+                onChange={onChange}
+              />
+            </div>
+          )}
+        />
 
-        <div className="flex flex-col gap-2">
-          <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
-            Link encurtado
-          </span>
-          <div className="flex align-center h-12 w-full px-4 py[0.9375rem] text-md border-[1px] border-gray-300 rounded-lg">
-            <span className="self-center h-fit text-gray-600">brev.ly/</span>
-            <input
-              className="w-full text-gray-600 focus-visible:outline-none"
-              spellCheck={false}
-            />
-          </div>
-        </div>
+        <Controller
+          control={control}
+          name={'shortUrl'}
+          render={({ field: { value, onChange } }) => (
+            <div className="flex flex-col gap-2">
+              <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
+                Link encurtado
+              </span>
+              <div className="flex align-center h-12 w-full px-4 py[0.9375rem] text-md border-[1px] border-gray-300 rounded-lg">
+                <span className="self-center h-fit text-gray-600">
+                  brev.ly/
+                </span>
+                <input
+                  value={value}
+                  className="w-full text-gray-600 focus-visible:outline-none"
+                  spellCheck={false}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+          )}
+        />
       </div>
 
-      <button className="h-12 text-md text-white font-(weight:--font-semibold) cursor-pointer bg-blue-base rounded-lg hover:brightness-101 ease-in-out">
+      <button
+        disabled={originalUrl.length === 0 || shortUrl.length === 0}
+        className="h-12 text-md text-white font-(weight:--font-semibold) cursor-pointer bg-blue-base rounded-lg hover:brightness-101 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-200 ease-in-out"
+      >
         Salvar link
       </button>
     </div>
