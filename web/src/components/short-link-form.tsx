968fe4319677eadd10699567a2bf4d2ac63fb26d
joao magnago <joaomagnago.wm@gmail.com>
2025-09-27 17:21:47 -0300

    refactor: add overflow to short links list and edit font theme

--- web/src/components/short-link-form.tsx
+++ web/src/components/short-link-form.tsx
@@ -27,9 +27,7 @@ export function ShortLinkForm() {
 
   return (
     <div className="flex flex-col p-8 gap-6 bg-gray-100 w-full rounded-lg">
-      <h2 className="text-xl/(line-height-xl) font-(weight:--font-bold) text-gray-600">
-        Novo link
-      </h2>
+      <h2 className="text-lg text-gray-600">Novo link</h2>
 
       <div className="flex flex-col gap-4">
         <Controller
@@ -37,12 +35,12 @@ export function ShortLinkForm() {
           name={'originalUrl'}
           render={({ field: { value, onChange } }) => (
             <div className="flex flex-col gap-2">
-              <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
+              <span className="text-xs text-gray-500 uppercase">
                 Link original
               </span>
               <input
                 value={value}
-                className="h-12 w-full px-4 py[0.9375rem] text-md text-gray-600 border-[1px] border-gray-300 rounded-lg focus-visible:outline-none placeholder:text-gray-400"
+                className="h-12 w-full px-4 py[0.9375rem] text-md-regular text-gray-600 border-[1px] border-gray-300 rounded-lg focus-visible:outline-none placeholder:text-gray-400"
                 placeholder="www.exemplo.com.br"
                 spellCheck={false}
                 onChange={onChange}
@@ -56,10 +54,10 @@ export function ShortLinkForm() {
           name={'shortUrl'}
           render={({ field: { value, onChange } }) => (
             <div className="flex flex-col gap-2">
-              <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
+              <span className="text-xs text-gray-500 uppercase">
                 Link encurtado
               </span>
-              <div className="flex align-center h-12 w-full px-4 py[0.9375rem] text-md border-[1px] border-gray-300 rounded-lg">
+              <div className="flex align-center h-12 w-full px-4 py[0.9375rem] text-md-regular border-[1px] border-gray-300 rounded-lg">
                 <span className="self-center h-fit text-gray-600">
                   brev.ly/
                 </span>
@@ -77,7 +75,7 @@ export function ShortLinkForm() {
 
       <button
         disabled={originalUrl.length === 0 || shortUrl.length === 0}
-        className="h-12 text-md text-white font-(weight:--font-semibold) cursor-pointer bg-blue-base rounded-lg hover:brightness-101 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-200 ease-in-out"
+        className="h-12 text-md-semibold text-white font-(weight:--font-semibold) cursor-pointer bg-blue-base rounded-lg hover:brightness-101 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-200 ease-in-out"
       >
         Salvar link
       </button>
--- web/src/components/short-links-list.tsx
+++ web/src/components/short-links-list.tsx
@@ -14,76 +14,81 @@ interface ShortLink {
 export const ShortLinksList = () => {
   const shortLinks: ShortLink[] = [
     {
-      originalUrl: 'www.exemplo.com.br',
-      shortUrl: 'brev.ly/exemplo',
-      accessCount: 10,
+      originalUrl: 'www.youtube.com.br',
+      shortUrl: 'brev.ly/Youtube-Subscriptions',
+      accessCount: 12,
     },
     {
-      originalUrl: 'www.exemplo.com.br',
-      shortUrl: 'brev.ly/exemplo',
-      accessCount: 10,
+      originalUrl: 'www.google.com.br',
+      shortUrl: 'brev.ly/Google-Search',
+      accessCount: 6,
     },
     {
-      originalUrl: 'www.exemplo.com.br',
-      shortUrl: 'brev.ly/exemplo',
-      accessCount: 10,
+      originalUrl: 'www.facebook.com.br',
+      shortUrl: 'brev.ly/Facebook-Page',
+      accessCount: 1,
     },
     {
-      originalUrl: 'www.exemplo.com.br',
-      shortUrl: 'brev.ly/exemplo',
-      accessCount: 10,
+      originalUrl: 'www.instagram.com.br',
+      shortUrl: 'brev.ly/Instagram-Feed',
+      accessCount: 37,
     },
   ];
 
   return (
     <div className="flex items-center flex-col gap-5 p-8 rounded-lg bg-gray-100 w-full lg:max-w-[580px] lg:mt-16">
       <div className="flex justify-between w-full">
-        <h2 className="text-xl/(line-height-xl) font-(weight:--font-bold) text-gray-600">
-          Meus links
-        </h2>
+        <h2 className="text-lg text-gray-600">Meus links</h2>
 
-        <button className="flex items-center p-2 rounded-sm cursor-pointer gap-1.5 bg-gray-200 hover:brightness-101 ease-in-out active:scale-99 transition-all duration-50">
+        <button
+          disabled={!shortLinks || shortLinks.length === 0}
+          className="flex items-center p-2 rounded-sm cursor-pointer gap-1.5 bg-gray-200 hover:brightness-101 ease-in-out disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-100"
+        >
           <DownloadSimpleIcon fontSize={16} />
-          <span className="text-sm font-(weight:--font-semibold) text-gray-500">
+          <span className="text-sm-semibold font-(weight:--font-semibold) text-gray-500">
             Baixar CSV
           </span>
         </button>
       </div>
 
-      {shortLinks.length > 0 ? (
-        <div className="flex items-center justify-between py-4 w-full border-t-1 border-t-gray-200">
-          <div className="flex flex-col justify-between">
-            <span className="text-md/(line-height-md) font-(weight:--font-semibold) text-blue-base">
-              www.youtube.com
-            </span>
-            <span className="text-sm/(line-height-sm) text-gray-500">
-              Youtube
-            </span>
-          </div>
+      <div className="flex flex-col w-full lg:max-h-[70vh] lg:overflow-y-auto">
+        {shortLinks.length > 0 ? (
+          shortLinks.map(({ shortUrl, originalUrl, accessCount }) => (
+            <div className="flex items-center justify-between py-4 w-full border-t-1 border-t-gray-200">
+              <div className="flex flex-col justify-between">
+                <span className="text-md-semibold font-(weight:--font-semibold) text-blue-base">
+                  {shortUrl}
+                </span>
+                <span className="text-sm-regular text-gray-500">
+                  {originalUrl}
+                </span>
+              </div>
 
-          <div className="flex items-center gap-5">
-            <span className="text-sm/(line-height-sm) text-gray-500 l">
-              30 acessos
-            </span>
-            <div className="flex items-center gap-1">
-              <button className="flex items-center justify-center p-2 rounded-sm bg-gray-200 hover:cursor-pointer active:scale-95 transition-all duration-100">
-                <CopyIcon />
-              </button>
-              <button className="flex items-center justify-center p-2 rounded-sm bg-gray-200 hover:cursor-pointer active:scale-95 transition-all duration-100">
-                <TrashIcon />
-              </button>
+              <div className="flex items-center gap-5">
+                <span className="text-sm-regular text-gray-500">
+                  {`${accessCount} ${accessCount === 1 ? 'acesso' : 'acessos'}`}
+                </span>
+                <div className="flex items-center gap-1">
+                  <button className="flex items-center justify-center p-2 rounded-sm bg-gray-200 hover:cursor-pointer active:scale-95 transition-all duration-100">
+                    <CopyIcon />
+                  </button>
+                  <button className="flex items-center justify-center p-2 rounded-sm bg-gray-200 hover:cursor-pointer active:scale-95 transition-all duration-100">
+                    <TrashIcon />
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ) : (
-        <div className="flex flex-col items-center gap-3 pt-4 pb-6 w-full border-t-1 border-t-gray-200">
-          <LinkIcon fontSize={32} />
+          ))
+        ) : (
+          <div className="flex flex-col items-center gap-3 pt-4 pb-6 w-full border-t-1 border-t-gray-200">
+            <LinkIcon fontSize={32} />
 
-          <span className="text-xs/(--line-height-xs) text-gray-500 uppercase">
-            Ainda não existem links cadastrados
-          </span>
-        </div>
-      )}
+            <span className="text-xs text-gray-500 uppercase">
+              Ainda não existem links cadastrados
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
